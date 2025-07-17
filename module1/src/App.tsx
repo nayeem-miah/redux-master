7d9c78b6a14628e7df1e8e14df40efd36865611c
nayeem-miah <nayeem5113a@gmail.com>
2025-07-17 20:17:17 +0600

    store create

--- module1/src/App.tsx
+++ module1/src/App.tsx
@@ -1,13 +1,25 @@
+import { useState } from "react"
 
 
 function App() {
+  const [count, setCount] = useState(0);
 
+  const handleIncrement = () => {
+    setCount(() => count + 1)
+  }
+  const handleDecrement = () => {
+    setCount(() => count - 1)
+  }
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Welcome to My App</h1>
+      <h1 className="text-3xl font-bold">Welcome to My App</h1>
       <p className="text-lg">This is a simple React application.</p>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => alert('Button clicked!')}>Click Me</button>
-      <p className="text-lg">Enjoy exploring the features!</p>
+
+      <div className="text-3xl">count {count}</div>
+      <div >
+        <button onClick={handleIncrement} className="bg-green-400 mr-2 p-2 rounded ">increment</button>
+        <button onClick={handleDecrement} className="bg-red-400 p-2 rounded ">decrement</button>
+      </div>
     </>
   )
 }
